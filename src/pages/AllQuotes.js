c685d0e8b1ecac4560ed5749bf0e284b4860f439
Natalia Rossini <naty.rossini@hotmail.com>
2021-07-20 17:24:23 +0100

    add error page, form redirect, nested routes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,4 +1,19 @@
+import QuoteList from '../components/quotes/QuoteList'
+
+const DUMMY_QUOTES = [
+    {
+        id: 'q1',
+        author: 'name',
+        text: 'lorem ipsum'
+    },
+    {
+        id: 'q2',
+        author: 'other name',
+        text: 'lorem ipsum dolor'
+    }
+]
+
 const AllQuotes = () => {
-    return <h1>All Quotes</h1>
+    return <QuoteList quotes={DUMMY_QUOTES} />
 }
 export default AllQuotes;
--- src/pages/QuoteView.js
+++ src/pages/QuoteView.js
@@ -1,11 +1,33 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+    {
+        id: 'q1',
+        author: 'name',
+        text: 'lorem ipsum'
+    },
+    {
+        id: 'q2',
+        author: 'other name',
+        text: 'lorem ipsum dolor'
+    }
+];
 
 const QuoteView = () => {
     const params = useParams();
+
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+    
+    if (!quote) {
+        return <p>No quote found</p>
+    }
+    
     return (
         <Fragment>
+            <HighlightedQuote text={quote.text} author={quote.author} />
             <h1>Quote View</h1>
             <p>{params.quoteId}</p>
             <Route path={`/quotes/${params.quoteId}/comments`}>
