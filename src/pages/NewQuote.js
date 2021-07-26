d1a37eb18412eb06fc6d9448286fd4ade5c501c6
Natalia Rossini <naty.rossini@hotmail.com>
2021-07-26 15:19:05 +0100

    fetch comments

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,21 @@
+import { useEffect } from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import useHttp from '../hooks/use-http'
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+    const  { sendRequest, status } = useHttp(addQuote)
     const history = useHistory();
+
+    useEffect(() => {
+        if (status === 'completed'){
+            history.push('/quotes')
+        }
+    }, [status, history]);
     const addQuoteHandler = quoteData => {
-        console.log(quoteData)
-        history.push('/quotes');
+        sendRequest(quoteData)
     }
-    return <QuoteForm onAddQuote={addQuoteHandler} />
+    return <QuoteForm isLoaging={status === 'pending'} onAddQuote={addQuoteHandler} />
 }
 export default NewQuote;
